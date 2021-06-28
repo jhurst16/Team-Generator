e86304f1a0abf249f7dd55fc79cb1a8812f5b71f
Jeremy's commits <jeremyhurst16@gmail.com>
2021-06-27 20:22:37 -0400

    fixed css and further styled html page

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
-const Employee = require('../lib/Employee');
+const Employee = require('../lib/Employee')
 const Manager = require('../lib/Manager')
 const Engineer = require('../lib/Engineer')
-const Intern = require('../lib/Intern');
+const Intern = require('../lib/Intern')
 const Index = require('../index')
 
 const boilerTop = `
@@ -12,19 +12,27 @@ const boilerTop = `
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+  integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <title>My Team Generator</title>
 </head>
-<body> 
+<body>
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title d-flex justify-content-center">My Team!</h5>
+  </div>
+</div>
+<div class="d-flex justify-content-around">
 `
-const boilerBottom =
-`
+const boilerBottom = `
+</div>
+<script src="../index.js"></script>
 </body>
 </html>
-`;
+`
 
-const generateNewManager = manager => {
-    return `
+const generateNewManager = (manager) => {
+  return `
 <div class="card" style="width: 18rem;">
 <div class="card-body">
     <h5 class ="card-title">${manager.name}</h5>
@@ -32,15 +40,15 @@ const generateNewManager = manager => {
     </div>
     <ul class ="list-group list-group-flush">
         <li class ="list-group-item">ID Number: ${manager.id}</li>
-        <li class ="list-group-item">Email: ${manager.email}</li>
+        <li class ="list-group-item">Email: <a href="mailto: ${manager.email}">${manager.email}</a></li>
         <li class ="list-group-item"> Office Number: ${manager.getOfficeNumber()}</li>
         </ul>
         </div>
-    `;
+    `
 }
 
-const generateNewEngineer = engineer => {
-    return `
+const generateNewEngineer = (engineer) => {
+  return `
 <div class="card" style="width: 18rem;">
 <div class="card-body">
     <h5 class ="card-title">${engineer.name}</h5>
@@ -50,13 +58,13 @@ const generateNewEngineer = engineer => {
         <li class ="list-group-item">ID Number: ${engineer.id}</li>
         <li class ="list-group-item">Email: ${engineer.email}</li>
         <li class ="list-group-item"> Github Account: <a href = "http://www.github.com/">${engineer.getGithub()}</li></a>
-        </ul>Office Number
+        </ul>
         </div>
-    `;
+    `
 }
 
-const generateNewIntern = intern => {
-    return `
+const generateNewIntern = (intern) => {
+  return `
 <div class="card" style="width: 18rem;">
 <div class="card-body">
     <h5 class ="card-title">${intern.name}</h5>
@@ -68,31 +76,27 @@ const generateNewIntern = intern => {
         <li class ="list-group-item"> School: ${intern.getSchool()}</li>
         </ul>
         </div>
-    `;
+    `
 }
 
-
-
-
 const FinalHTML = (newTeamMember) => {
-    let createHTML = boilerTop
-    for (let i = 0; i < newTeamMember.length; i++) {
-        let element = newTeamMember[i];
-        // console.log(element)
-        if (element instanceof Manager) {
-        createHTML = createHTML + generateNewManager(element)
-        } else if(element instanceof Engineer){
-            createHTML = createHTML + generateNewEngineer(element)
-        } else if (element instanceof Intern) {
-            createHTML = createHTML + generateNewIntern(element)
-        } else {
-            console.log('no team members have been added.')
-        }
+  let createHTML = boilerTop
+  for (let i = 0; i < newTeamMember.length; i++) {
+    let teamClass = newTeamMember[i]
+    // console.log(element)
+    if (teamClass instanceof Manager) {
+      createHTML = createHTML + generateNewManager(teamClass)
+    } else if (teamClass instanceof Engineer) {
+      createHTML = createHTML + generateNewEngineer(teamClass)
+    } else if (teamClass instanceof Intern) {
+      createHTML = createHTML + generateNewIntern(teamClass)
+    } else {
+      console.log('no team members have been added.')
     }
-    createHTML = createHTML + boilerBottom
-    // console.log(createHTML)
-    return createHTML;
+  }
+  createHTML = createHTML + boilerBottom
+  // console.log(createHTML)
+  return createHTML
 }
 
 module.exports = FinalHTML
-
